e7e7bbc0a45cd999ae7228652d95ddfe9063e4ae
Nori <80853209+Ankokukun@users.noreply.github.com>
2022-10-30 14:21:29 +0100

    Update pong.js
    fixed ball Velocity only being negative
    
    added collision between paddle and world border

--- assets/js/pong.js
+++ assets/js/pong.js
@@ -2,6 +2,7 @@ var config = {
     width: 1024,
     height: 768,
     backgroundColor: 0x000000,
+    type:Phaser.AUTO,
     scene: {
         create: create,
         update: update
@@ -11,9 +12,20 @@ var config = {
     }
 }
 
-//  TO DO
-//  center canvas
+//  TO DO GENEREL
+//  xcenter canvas
 //  get sidebar working
+//  Tic Tac Toe
+
+
+// TO DO PONG
+//// get random direction of ball fixed --> additional randomizer, that changes - to +
+//   pause screen --> have to add multiple scenes and switch back and forth
+//   fix collision of paddle and ball --> add timer for repelling the ball
+//// fix collision of paddle with worldbounds
+//// put both randomV functions into one
+//   add soundeffects
+//   fix ScoreCounter
 
 var game = new Phaser.Game(config);
 
@@ -27,125 +39,124 @@ var ballV = {
 var paddleLeft;
 var paddleRight;
 
+function getRandomFloat(min, max, decimals) {
+  const str = (Math.random() * (max - min) + min).toFixed(decimals);
+
+  return parseFloat(str);
+}
+
+function randomV()
+{
+  let randomFloat;
+  let worked = false;
+  while (!worked) {
+    randomFloat = getRandomFloat(-2,2,2);
+    if(randomFloat <= -0.5) //only negative numbers
+    {
+      if(randomFloat >= 0.5)
+      {
+        worked = true;
+      }
+      break;
+    }
+  }
+  if(getRandomFloat(0,1,0) == 0)
+  {
+    return randomFloat;
+  }
+  else return -randomFloat; 
+}
+
+
+function resetBall()
+  {
+    ball.x= 512
+    ball.y= 334
+    ballV.x= randomV()
+    console.log("BallV.x: "+ballV.x)
+    ballV.y= randomV()
+    console.log("BallV.y: "+ballV.y)
+  }
+
+var leftScore = 0;
+var leftScoreText;
+
+var rightScore = 0;
+var rightScoreText;
+
 function create()
 {
     paddleLeft = this.add.rectangle(50,config.height/2-75/2,15,75,0xffffff);
-    paddleLeft = this.add.rectangle(config.width-50,config.height/2-75/2,15,75,0xffffff);
+    paddleRight = this.add.rectangle(config.width-50,config.height/2-75/2,15,75,0xffffff);
     ball = this.add.circle(512,334,12,0xffffff);
+
+    leftScoreText = this.add.text(100, 10, 'Score: ', { fontFamily: 'Georgia, "Goudy Bookletter 1911", Times, serif' });
+  	rightScoreText = this.add.text(config.width - 150, 10, 'Score: ', { fontFamily: 'Georgia, "Goudy Bookletter 1911", Times, serif' });
+
+    //leftScoreText.setText('Score: '+leftScore);
+    //rightScoreText.setText('Score: '+rightScore);
 
     this.physics.add.existing(paddleLeft, false);
     this.physics.add.existing(paddleRight, false);
     this.physics.add.existing(ball,false);
     this.physics.add.collider(paddleLeft,ball,collisionHandler);
     this.physics.add.collider(paddleRight,ball,collisionHandler);
+
+    paddleLeft.body.setCollideWorldBounds(true);
+    paddleRight.body.setCollideWorldBounds(true);
+
+    ball.body.setCollideWorldBounds(true);
+    ball.body.onWorldBounds = true;
+    this.physics.world.on('worldbounds', function(body, up, down, left, right)
+    {
+      if(up||down)
+      {
+        ballV.y = -ballV.y;
+      }
+      if(left)
+      {
+        this.rightScore ++; //score doesn't update
+        rightScoreText.setText('Score: '+rightScore);
+        resetBall();
+        //this.scene.pause();
+      }
+      if(right)
+      {
+        this.leftScore ++;
+        leftScoreText.setText('Score: '+leftScore);
+        resetBall();
+        //this.scene.pause();
+      }
+    });
+    resetBall();
 }
 
 //collision
 
 function collisionHandler(_pPaddel)
 {
-  if(_pPaddel = paddleLeft)
+  if(_pPaddel == paddleLeft)
   {
     ball.x = paddleLeft.x + paddleLeft.width + ball.radius
     ballV.x = - ballV.x
     ballV.y = - ballV.y
-    //this.randomV();
   }
   else
   {
     ball.x = paddleRight.x - paddleRight.width - ball.radius
     ballV.x = - ballV.x
     ballV.y = - ballV.y
-    //this.randomV();
   }
 }
 
-
-//wallCollision:
-/*
-    in create() -->   Velocity(100, 200).setBounce(1, 1).setCollideWorldBounds(true)
-    or:
-    var group = this.physics.add.group({
-        key: 'ball',
-        frameQuantity: 48,
-        bounceX: 1,
-        bounceY: 1,
-        collideWorldBounds: true,
-        velocityX: 180,
-        velocityY: 120,
-    });
-*/
-/*
-    my approach:
-
-    create function for each side
-
-    function leftSideCheck()
-    {
-      return ball.x =< 0
-    }
-
-    function rightSideCheck()
-    {
-      return ball.x => config.width
-    }
-
-    function that checks whether one is true, and updates scorecount and resets scene
-    
-    function wallCheck()
-    {
-      if(leftSideCheck())
-      {
-        this.leftScore += 1;
-        this.resetBall();
-        this.scene.pause();
-      }
-      else if(leftSideCheck())
-      {
-        this.rightScore += 1;
-        this.resetBall();
-        this.scene.pause();
-      }
-    }
-
-*/
-
-//reset ball (and paddles as well)
-/*
-  function resetBall()
-  {
-    ball.x= 512
-    ball.y= 334
-    ballV.x= this.randomV()
-    ballV.y= this.randomV()
-  }
-
-*/
-
-//random Velocity:
-/*
-    function randomV()
-    {
-      return Math.floor(Math.random() * (4 - -3) ) + -3;
-    }
-*/
-
-//Score:
-/*
-    var leftScore;
-    var leftScoreText = this.add.text(50, 10, 'Score:'+ leftScore, { fontFamily: 'Georgia, "Goudy Bookletter 1911", Times, serif' });
-    var rightScore;
-    var rightScoreText = this.add.text(config.width - 50, 10, 'Score:'+ rightScore, { fontFamily: 'Georgia, "Goudy Bookletter 1911", Times, serif' });
-
-*/
 
 function update() 
 {
   
   let wasd = this.input.keyboard.addKeys('W,S');
   let cursors = this.input.keyboard.createCursorKeys();
-  let enter = this.input.keyboard.addKeys(13);
-  let escape = this. input.keyboard.addKeys(27);
+  //let enter = this.input.keyboard.addKeys(13);
+  let escape = this.input.keyboard.addKey(27);
 
   if (wasd.S.isDown)
   {
@@ -164,19 +175,16 @@ function update()
   {
       paddleRight.y -= 5;
   }
-  
-  if (enter.isDown)
+/*  can't resume a scene if it's paused, and doesn't react to keypresses
+  if(escape.isDown)
   {
-    if(game.scene.isPaused)
-    {
+    console.log(this.scene.isPaused("default"));
+    if(this.scene.isPaused("default")) {
+      console.log("game resumed"); 
       this.scene.resume();
-    }
+    } else console.log("game paused"); this.scene.pause();
   }
-
-  if (escape.isDown)
-  {
-    this.scene.pause();
-  }
+*/
 
   ball.x += ballV.x;
   ball.y += ballV.y;
